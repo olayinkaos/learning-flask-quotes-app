bfbef82ef319caeda102178e1278307839005c51
Olayinka Success <olayinka.omole@gmail.com>
2017-02-11 12:15:53 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, render_template, request
+from quotes import PERSONS
+
+app = Flask(__name__)
+
+def getQuotes(person, limit):
+	try:
+		person = person.lower()
+		person = person.replace(' ', '_')
+		quotes = PERSONS.get(person)
+		if limit and limit != '0':
+			try:
+				limit = int(limit)
+				quotes = quotes[:limit]
+			except ValueError:
+				quotes = quotes
+	except IndexError:
+		quotes = []
+	return quotes
+
+
+@app.route('/', methods = ['GET'])
+def index():
+	person = request.args.get('person', '')
+	limit = request.args.get('limit', '')
+	quotes = getQuotes(person, limit)
+	context = { 'person': person, 'quotes': quotes, 'limit': limit }
+	return render_template('index.html', **context)
+
+app.run(debug=True)
---
+++ quotes.py
@@ -0,0 +1,52 @@
+PERSONS = {
+    "trump": [
+        "All of the women on The Apprentice flirted with me - consciously or unconsciously. That's to be expected.",
+        "What separates the winners from the losers is how a person reacts to each new twist of fate",
+        "Sometimes your best investments are the ones you don't make.",
+        "In the end, you're measured not by how much you undertake but by what you finally accomplish.",
+        "You have to think anyway, so why not think big?",
+        "If you're interested in 'balancing' work and pleasure, stop trying to balance them. Instead make your work more pleasurable.",
+        "Everything in life is luck."
+    ],
+    "drake": [
+        "Live without pretending, Love without depending, Listen without defending, Speak without offending.",
+        "Kill them with success and bury them with a smile.",
+        "Haters will broadcast your failures, but whisper your success.",
+        "People will wish you all the success in the world and then hate you when you get it.",
+        "My only wish is I die real.",
+        "Smile and let everyone know that today, you're a lot stronger than you were yesterday.",
+        "Everybody dies, but not everbody lives.",
+        "I'm trying to do better than good enough"
+    ],
+    "alicia_keys": [
+        "Your life can become the background for the art you create.",
+        "Wait, I'm deciding to be my own individual self. And it looks nothing like what anyone else is doing.",
+        "Everything you want to be, you already are. You're simply on the path to discovering it.",
+        "I always want to stay focused on who I am, even as I'm discovering who I am.",
+        "We listen so much to everybody - more than ever, because we have a kabillion voices whose opinion we can access - and we care so much if everybody agrees with us. To bust through all of the noise is very challenging.",
+        "Empower us women and we will change this whole entire world.",
+        "Be driven, be focused, but enjoy every moment, because it only happens once.",
+        "I think what happens in the world, and I think it's part human nature and part programming, is we become an emulation of what we see. We become clones of each other."
+    ],
+    "abel_tesfaye": [
+        "For a dreamer, nights the only time of day.",
+        "Be loyal to your own piece of mind.",
+        "I know you've been trying to get a hold of me, but I can barely get a hold of myself",
+        "I'm too full to swallow my pride.",
+        "You know me, but you don't know me. I give you what I want to give you. I relate to villains like that - but I'm not out to destroy the world.",
+        "Yeah, people always talk about the ones that got away, I just seem to get the ones that want to stay.",
+        "Happiness exist when you don't know a thing",
+        "They asked me why I don't quit drinking, I told 'em I'm not a quitter.",
+        "Staying in a hopeless and miserable situation doesn't make you loyal, it just makes you miserable.",
+        "What does it mean when your heart's already numb? You're a professional."
+    ],
+    "buhari": [
+        "I belong to everyone, and I belong to no one.",
+        "I don't know which party my wife belongs to, but she belongs to my kitchen, and my living room and the other room.",
+        "People have been voting to choose their leader but their votes were never allowed to COUNT, I will not allow the choice of the people to be subverted by any party, not even APC",
+        "We cannot build an economy where corruption is the working capital",
+        "I will continue to show openly and inside me the total commitment to the Sharia movement that is sweeping all over Nigeria God willing, we will not stop the agitation for the total implementation of the Sharia in the country.",
+        "Boko Haram will soon know the strength of our collective will",
+        "A few people have privately voiced fears that on coming back to office I shall go after them. These fears are groundless. There will be no paying off old scores. The past is prologue."
+    ]
+}
